a964ad86ffb71914da20060b84b14810e6c0280d
Fabian <fabi.winkler@t-online.de>
2019-12-28 11:26:40 +0100

    third commit - theme updated and libraries included

--- app/src/main/java/com/example/ap_assignment/MainActivity.kt
+++ app/src/main/java/com/example/ap_assignment/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.ap_assignment
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        info("Activity Started")
     }
+
+
 }
