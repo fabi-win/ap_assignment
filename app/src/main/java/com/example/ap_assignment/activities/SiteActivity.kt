bda2dcd1cba51941ba3a932f122b4fcea9e92730
Fabian <fabi.winkler@t-online.de>
2019-12-28 15:00:53 +0100

    model defined and onclicklistener of button

--- app/src/main/java/com/example/ap_assignment/activities/SiteActivity.kt
+++ app/src/main/java/com/example/ap_assignment/activities/SiteActivity.kt
@@ -4,11 +4,14 @@ package com.example.ap_assignment.activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ap_assignment.R
+import com.example.ap_assignment.models.SiteModel
 import kotlinx.android.synthetic.main.activity_site.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
 class SiteActivity : AppCompatActivity(), AnkoLogger {
+
+    var site = SiteModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +19,11 @@ class SiteActivity : AppCompatActivity(), AnkoLogger {
         info("Activity Started")
 
         btnAdd.setOnClickListener(){
-
+            site.title = siteTitle.text.toString()
+            site.descripton = siteDescription.text.toString()
+            if(site.title.isNotEmpty()){
+                info("addButtonPressed: $site")
+            }
         }
     }
 
--- app/src/main/java/com/example/ap_assignment/models/SiteModel.kt
+++ app/src/main/java/com/example/ap_assignment/models/SiteModel.kt
@@ -1,2 +1,7 @@
 package com.example.ap_assignment.models
 
+import java.io.FileDescriptor
+
+data class SiteModel(var title: String ="",
+                     var descripton: String ="",
+                     var image: String ="")
