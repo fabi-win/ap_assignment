9bd511ba69f3e1f66b6eb162cffd3158cb50cd82
Fabian <fabi.winkler@t-online.de>
2019-12-28 11:46:32 +0100

    fourth commit - button and textview

--- app/src/main/java/com/example/ap_assignment/SiteActivity.kt
+++ app/src/main/java/com/example/ap_assignment/SiteActivity.kt
@@ -1,19 +1,22 @@
 package com.example.ap_assignment
 
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_site.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity(), AnkoLogger {
+class SiteActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_site)
         info("Activity Started")
-    }
 
+        btnAdd.setOnClickListener(){
+
+        }
+    }
 
 }
